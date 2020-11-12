3a724e242e5f3f476cc90366bee86c459e9c71b1
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-11-12 17:04:47 +0100

    build

---
+++ migrations/2_deploy_contract.js
@@ -0,0 +1,9 @@
+const web3 = require("@openzeppelin/test-helpers/src/config/web3");
+
+const Wallet = artifacts.require("Wallet");
+
+module.exports = async function (deployer, _network, accounts) {
+    await deployer.deploy(Wallet, [accounts[0], accounts[1], accounts[2]], 2);
+    const wallet = await Wallet.deployed();
+    await web3.eth.sendTransaction({ from: accounts[0], to: wallet.address, value: 100000 });
+};
