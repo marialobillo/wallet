e6f99e58d2839c8f0f2e474971789f3cd0ea4351
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-11-14 13:47:41 +0100

    setting up the provider for kovan network

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const provider = require('@truffle/hdwallet-provider');
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -39,42 +39,19 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+    kovan: {
+      provider: () => 
+        new provider(
+          ['fa9b313d8fb4bd303dc86ad0b5b6189008083e9beea8dd76b3289f9d7259246e',
+          'fcfd24e343132867076d726a4454dc231de48b4afe2bedda91357e7d78dba1fe',
+          'a0208392e07a462583a92f9ce8ff9149ba692a1a521b09116403397b0501344b'
+          ],
+          'https://kovan.infura.io/v3/6f196fa725744f50ba5e728ce5701c3b',
+          0,
+          3
+        ),
+        network_id: 42
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
